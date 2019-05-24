abc0cb70066c9d1d0680bd261387fb2ec40c4543
mnalpert <mnalpert1@gmail.com>
2019-05-24 13:14:21 -0400

    add test for cached property

---
+++ tests/test_utils.py
@@ -0,0 +1,29 @@
+import mock
+
+from nfl_survivor.utils import cached_property
+
+
+def compute():
+    return 1
+
+
+class C:
+
+    @cached_property
+    def cp(self):
+        return compute()
+
+
+@mock.patch('tests.test_utils.compute')
+def test_cached_property(mocked_compute):
+    mocked_compute.return_value = 1
+
+    c = C()
+
+    # first time we call compute
+    assert c.cp == 1
+    assert mocked_compute.call_count == 1
+
+    # second time we don't so call count remains 1
+    assert c.cp == 1
+    assert mocked_compute.call_count == 1
