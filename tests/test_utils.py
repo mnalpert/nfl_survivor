7d7330b5176320db2ea70c4c3ca1b32f1a2b5d75
mnalpert <mnalpert1@gmail.com>
2019-05-24 15:56:16 -0400

    add regular property behavior as comparison

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,6 +13,10 @@ class C:
     def cp(self):
         return compute()
 
+    @property
+    def rp(self):
+        return compute()
+
 
 @mock.patch('tests.test_utils.compute')
 def test_cached_property(mocked_compute):
@@ -27,3 +31,18 @@ def test_cached_property(mocked_compute):
     # second time we don't so call count remains 1
     assert c.cp == 1
     assert mocked_compute.call_count == 1
+
+
+@mock.patch('tests.test_utils.compute')
+def test_regular_property(mocked_compute):
+    mocked_compute.return_value = 1
+
+    c = C()
+
+    # first time we call compute
+    assert c.rp == 1
+    assert mocked_compute.call_count == 1
+
+    # second time we don't so call count remains 1
+    assert c.rp == 1
+    assert mocked_compute.call_count == 2
