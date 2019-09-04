01f4b4edc62d14c65e3bb41424ded5923de0e9d5
mnalpert <mnalpert1@gmail.com>
2019-09-03 23:37:46 -0400

    change picker option to lp | greedy. add some seasons

--- nfl_survivor/make_picks.py
+++ nfl_survivor/make_picks.py
@@ -17,8 +17,8 @@ PICKERS = (GreedyPicker, LpPicker)
 @click.command()
 @click.option('-pp', '--previous_picks', 'previous_picks', type=str, default=None,
               help='File path to YAML file with previous picks')
-@click.option('-p', '--picker', 'picker_name', type=str, default='LpPicker',
-              help='Name of picker to use when making picks')
+@click.option('-p', '--picker', 'picker_name', type=str, default='lp',
+              help='Name of picker to use when making picks (lp | greedy)')
 @click.option('-o', '--output', 'output', type=str, default=None,
               help='YAML file path to write picks to')
 @click.option('-s', '--season', 'season_path', type=str,
@@ -30,7 +30,7 @@ def make_picks(season_path, output, picker_name, previous_picks):
     previous_picks = Picks.from_yaml(previous_picks) if previous_picks else None
     try:
         picker = {picker.__name__.lower(): picker
-                  for picker in PICKERS}[picker_name.lower()]
+                  for picker in PICKERS}[picker_name.lower() + 'picker']
     except KeyError:
         exception_msg = f'No picker matching {picker_name}'
         logger.exception(exception_msg)
